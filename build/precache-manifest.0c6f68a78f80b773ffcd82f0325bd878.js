d4afd73fdd6a17c6376bbe4b4b5ecb950ed61f97
Lucas Goncalves <lucasgoncalves@Lucass-MacBook-Pro.local>
2020-11-02 14:11:39 -0300

    build

---
+++ build/precache-manifest.0c6f68a78f80b773ffcd82f0325bd878.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875ca161d13e648df5b8fce4c8eb28ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf04fd12f00054d34be",
+    "url": "/static/css/2.8db5677f.chunk.css"
+  },
+  {
+    "revision": "f4cd6c4e0060d270c44f",
+    "url": "/static/css/main.72d4fa60.chunk.css"
+  },
+  {
+    "revision": "8bf04fd12f00054d34be",
+    "url": "/static/js/2.5f22e45b.chunk.js"
+  },
+  {
+    "revision": "f4cd6c4e0060d270c44f",
+    "url": "/static/js/main.ca2979f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "41b1597332603791c9f66cb6a1307cd1",
+    "url": "/static/media/addCircle.41b15973.svg"
+  },
+  {
+    "revision": "c18302f28b5b6e972a185f7fcfaeb050",
+    "url": "/static/media/icon-kanban-selected.c18302f2.svg"
+  },
+  {
+    "revision": "7967f733006c0120f26c8a4e712489db",
+    "url": "/static/media/icon-kanban.7967f733.svg"
+  },
+  {
+    "revision": "5ca457ebda80494907c401e3cf7c3e6a",
+    "url": "/static/media/icon-list-selected.5ca457eb.svg"
+  },
+  {
+    "revision": "60c7ac97698f38453a869324b436d225",
+    "url": "/static/media/icon-list.60c7ac97.svg"
+  },
+  {
+    "revision": "f92836b1f26e851f8b94e45365dcb148",
+    "url": "/static/media/logo.f92836b1.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
